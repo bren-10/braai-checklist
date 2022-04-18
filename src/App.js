7a34200d7b2472bce98ba2e20d1605777de56c89
Brendan Stander <info@bren-10.co.za>
2022-04-18 11:54:16 +0200

    Initial commit, start basic styling & add list items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        
       </header>
+
+      <div className='main-container'>
+        <h3>My Checklist</h3>
+        <hr/>
+        <div className='checklist-container'>
+          <h4>The standard</h4>
+          <ul>
+            <li>Dop</li>
+            <li>Chop</li>
+            <li>Boerewors</li>
+            <li>Sosaties</li>
+            <li>Salad (Potentially N/A)</li>
+            <li>Cigarettes/Vape-stuff</li>
+            <li>Wood</li>
+            <li>Charcoal</li>
+            <li>Firelighters</li>
+            <li>Matches</li>
+            <li>Ice</li>
+            <li>Snacks</li>
+          </ul>
+          <h4>The "better-be-over-prepared"</h4>
+          <ul>
+            <li>Hat</li>
+            <li>Sunscreen</li>
+            <li>BT Speaker</li>
+            <li>Music Playlist</li>
+            <li>Swim Cozzie</li>
+            <li>Towel</li>
+            <li>Gazebo and/or Umbrella</li>
+            <li>Monopoly</li>
+            <li>Acoustic Guitar (Don't be that person)</li>
+            <li>Beer Pong Cups</li>
+            <li>Cards</li>
+            <li>Paper Plates</li>
+            <li>Apron</li>
+          </ul>
+        </div>
+      </div>
+      
     </div>
   );
-}
+} 
 
 export default App;
